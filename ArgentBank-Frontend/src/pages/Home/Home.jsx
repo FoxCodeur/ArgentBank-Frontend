88be8398df4259f4b5b7756ab3c060a12089c2df
Daisy <detcherry@gmail.com>
2025-05-02 16:02:50 +0200

    dernier commit

--- ArgentBank-Frontend/src/pages/Home/Home.jsx
+++ ArgentBank-Frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,30 @@ import iconSecurity from "../../assets/img/icon-security.svg";
 import "./Home.scss";
 import FeatureItem from "../../components/FeatureItem/FeatureItem";
 
+const featuresData = [
+  {
+    icon: iconChat,
+    alt: "Chat Icon",
+    title: "You are our #1 priority",
+    description:
+      "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    icon: iconMoney,
+    alt: "Money Icon",
+    title: "More savings means higher rates",
+    description:
+      "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    icon: iconSecurity,
+    alt: "Security Icon",
+    title: "Security you can trust",
+    description:
+      "We use top of the line encryption to make sure your data and money is always safe.",
+  },
+];
+
 const Home = () => {
   return (
     <main className="home">
@@ -21,26 +45,15 @@ const Home = () => {
       <section className="features">
         <h2 className="sr-only">Features</h2>
 
-        <FeatureItem
-          icon={iconChat}
-          alt="Chat Icon"
-          title="You are our #1 priority"
-          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-        />
-
-        <FeatureItem
-          icon={iconMoney}
-          alt="Money Icon"
-          title="More savings means higher rates"
-          description="The more you save with us, the higher your interest rate will be!"
-        />
-
-        <FeatureItem
-          icon={iconSecurity}
-          alt="Security Icon"
-          title="Security you can trust"
-          description="We use top of the line encryption to make sure your data and money is always safe."
-        />
+        {featuresData.map((feature, index) => (
+          <FeatureItem
+            key={index}
+            icon={feature.icon}
+            alt={feature.alt}
+            title={feature.title}
+            description={feature.description}
+          />
+        ))}
       </section>
     </main>
   );
